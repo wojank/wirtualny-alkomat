a1d5aa42d0b4bbab0c603185d5768319814faf64
Poncjusz Miglanc <poncjuszmiglanc@gmail.com>
2023-02-14 20:12:18 +0100

    fourth commit, after final result function was built

--- src/components/TheStep3.vue
+++ src/components/TheStep3.vue
@@ -1,6 +1,22 @@
 <template>
-  <h1>Step 3 Wyniki:</h1>
-  <p>Twój wynik to: <span>Ostra Bomba </span></p>
+  <h1>Twój wynik:</h1>
+  <h2>Orientacyjna ilość promili w organiźnie: {{ props.promileResult }}</h2>
+  <p>ilość czystego alkoholu: {{ gramyResult }}</p>
+  <p>ilosć porcji standardowych: {{ standardResult }}</p>
+  <h3>Szacunkowy czas trzeźwienia: {{ fixedSoberTimeResult }}</h3>
 </template>
-<script setup></script>
+<script setup>
+import { defineProps, computed } from "vue";
+const props = defineProps([
+  "promileResult",
+  "gramyResult",
+  "standardResult",
+  "soberTimeResult",
+]);
+
+const fixedSoberTimeResult = computed(() => {
+  const wynik = props.soberTimeResult.toString().split(".");
+  return `${wynik[0]} godzin ${wynik[1]} minut`;
+});
+</script>
 <style src="" lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,15 @@
       <TheStep2 @action2="handleAction2" />
     </template>
     <template #step-3>
-      <TheStep3 />
+      <TheStep3
+        :promile-result="promileResult"
+        :gramy-result="gramyResult"
+        :standard-result="standardResult"
+        :sober-time-result="soberTimeResult"
+      />
     </template>
   </MainKalkulator>
   <button @click="alcoholCounter">Hej to ja</button>
-  <h1>{{ ml }}</h1>
 </template>
 
 <script setup>
@@ -26,59 +30,55 @@ const steps = ref([
   { step_no: 1, step_valid: false },
   { step_no: 1, step_valid: false },
 ]);
-//najpierw stan dla każdego z osobna, potem sprawdzę czy można to zrobić w ramach jednego obiektu
+
+let promileResult = ref("");
+let gramyResult = ref("");
+let standardResult = ref("");
+let soberTimeResult = ref("");
+
 let dataSex = ref("");
 let dataWeight = ref(0);
+
 const handleAction1 = (x, y) => {
   dataSex.value = y;
   dataWeight.value = x;
-  console.log("(HomeView) payload z (Step1) : ", x, y);
-  console.log("(HomeView) stan1 : ", dataSex, dataWeight);
 };
+
 let dataType = ref("");
 let dataPower = ref(0);
 let dataVolume = ref(0);
 let dataAmount = ref(0);
+
 const handleAction2 = (w, x, y, z) => {
   dataType.value = w;
   dataPower.value = x;
   dataVolume.value = y;
   dataAmount.value = z;
-  console.log("(HomeView) payload z (Step2) : ", w, x, y, z);
-  console.log(
-    "(HomeView) stan2 : ",
-    dataType,
-    dataPower,
-    dataVolume,
-    dataAmount
-  );
 };
+
+const alcoholCounter = () => {
+  const ml = dataVolume.value * dataAmount.value;
+  const A = ml * (dataPower.value / 100) * 0.8;
+  const K = dataSex.value == "mężczyzna" ? 0.7 : 0.6;
+  const W = dataWeight.value;
+  const result = A / (K * W);
+  const S = dataSex.value == "mężczyzna" ? 11 : 9;
+  soberTimeResult.value = (A / S).toFixed(2);
+  standardResult.value = A / 10;
+  gramyResult.value = A;
+  promileResult.value = result.toFixed(2);
+  return result;
+};
+
 // P = A / (K * W)
-// P - zawartość alkoholu
+// P - zawartość alkoholu w promilach
 // A - całkowita ilość wypitego alkoholu w gramach
 // K - współczynnik (kobiety : 0,6)(mężczyźni: 0,7)
 // W - masa ciała w kg
 //  A = ml * (%/100) * 0,8
-// let ml = computed(() => {
-//   return dataVolume.value * dataAmount.value;
-// });
-// let alcoholCounter = () => {
-//   console.log(dataVolume, dataAmount, ml.value);
-// };
-const alcoholCounter = () => {
-  const ml = dataVolume.value * dataAmount.value;
-  console.log("(HomeView) Ilość ml", ml);
-  const A = ml * (dataPower.value / 100) * 0.8;
-  console.log("(HomeView) Zmienna A", A);
-  const K = dataSex.value == "mężczyzna" ? 0.7 : 0.6;
-  console.log("(HomeView) Płeć: ", dataSex.value);
-  console.log("(HomeView) Zmienna K", K);
-  const W = dataWeight.value;
-  console.log("(HomeView) Zmienna W", W);
-  const result = A / (K * W);
-  console.log("(HomeView) Główny wynik", result);
-  return result;
-};
+
+//mezczyzna pali 10-12g/h , kobieta 8-10g/h czyli m=11g k=9g
+
 //potem będzie przynajmniej jeszcze jedna funkcja obliczająca jak długo będziemy trzeźwieć
 //i prawdopodobnie kolejny step z danymi, które według mojego uzania zmodyfikują współczynnik K jak np stan zoladka i dlugosc picia
 //wyniki też być może rozszerzyć tak jak są w pijodpowiedzialnie.pl czyli alko w g i w ml i promilach i szacunkowy czas trzezwienia i porcje standardowe
